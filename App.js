ea840e66e4e209ff1fbcd89c787f53cbafee15eb
Waqaruddin <syedwaqar.uddin@outlook.com>
2020-09-09 11:44:14 -0400

    Tab navigation with 3 tabs

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, View } from 'react-native';
+
+
+function HomeScreen(){
+  return(
+
+    <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'#FF8C00'}}>
+      <Text style ={{fontWeight:'bold', fontSize:32}}> Home Screen !</Text>
+    </View>
+  );
+}
+
+function SettingsScreen(){
+  return(
+    <View style = {{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'#37FDFC'}}>
+      <Text style ={{fontWeight:'bold', fontSize:32}}> Settings !</Text>
+    </View>
+  );
+}
+
+function UploadScreen(){
+  return(
+    <View style ={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'#FFDF00'}}>
+      <Text style ={{fontWeight:'bold', fontSize:32}}>Upload Screen ! </Text>
+    </View>
+  )
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name ="Home" component={HomeScreen}/>
+        <Tab.Screen name ="Settings" component= {SettingsScreen} />
+        <Tab.Screen name ="Upload" component={UploadScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
